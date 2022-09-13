916d24076413d5645c538cd3dc0770018f9f78ee
philito1 <64923997+philito1@users.noreply.github.com>
2022-09-13 23:53:02 +0100

    stable

--- scripts.js
+++ scripts.js
@@ -1,4 +1,20 @@
-const elem = document.getElementById('shirt');
+let time = 0;
+
+function getMarTop() {
+  return document.getElementById('shirt').style.marginTop;
+}
+
+function setMarTop(val) {
+  document.getElementById('shirt').style.marginTop = val;
+}
+
+function sineMarTop() {
+  newMar = 4 * Math.floor(6 * Math.sin(0.1 * time)) + 240;
+  setMarTop(String(newMar) + 'px');
+  time += 1;
+}
+
+setInterval(sineMarTop, 100);
 
 // while (true) {
 //   elem.style.paddingTop += 1;
